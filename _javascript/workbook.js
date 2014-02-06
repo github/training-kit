91ed5527a7d4385921361a4d8666c465e58934e7
jordanmccullough <jordan@github.com>
2014-02-06 13:16:57 -0700

    Add first pass of "scope" styles hide/show functionality

---
+++ _javascript/workbook.js
@@ -0,0 +1,51 @@
+(function(){
+  var scopeRadios = [];
+
+  scopeRadios.push(document.getElementById("outline"));
+  scopeRadios.push(document.getElementById("summary"));
+  scopeRadios.push(document.getElementById("details"));
+
+  for(var i=0; i<scopeRadios.length; i++){
+    scopeRadios[i].addEventListener("click", kaboom);
+  }
+
+  function kaboom(event){
+    var activeId = event.target.id;
+
+    switch(activeId){
+      case "outline":
+        showSet("outline");
+        hideSet("summary");
+        hideSet("details");
+        break;
+      case "summary":
+        hideSet("outline");
+        showSet("summary");
+        hideSet("details");
+        break;
+      case "details":
+        showSet("outline");
+        showSet("summary");
+        showSet("details");
+        break;
+    }
+  }
+  function hideSet(id){
+    var allEl = selection(id);
+        hideClassNames = id + " scope-hidden";
+    for(var e=0; e<allEl.length; e++){
+      allEl[e].className = hideClassNames;
+    }
+  }
+  function showSet(id){
+    var allEl = selection(id),
+        showClassNames = id + " scope-visible";
+    for(var e=0; e<allEl.length; e++){
+      allEl[e].className = showClassNames;
+    }
+  }
+  function selection(identifier){
+    var allEl = document.getElementsByClassName(identifier);
+    return allEl;
+  }
+})();
