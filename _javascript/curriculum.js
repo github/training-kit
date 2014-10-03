6268fde01489fe6fde7d96999c8f11038cf8ee11
jordanmccullough <jordan@github.com>
2014-10-02 22:28:48 -0600

    Add conditionals around "teacher" info injection

--- _javascript/curriculum.js
+++ _javascript/curriculum.js
@@ -19,6 +19,7 @@ $(function(){
 		{
 			url: "https://api.github.com/users/"+username,
 			success: function(data, textStatus, jqXHR){
+
 				$("<span/>",
 				{
 					class: "teacher-name",
@@ -30,20 +31,39 @@ $(function(){
 					text: data.login
 				}).appendTo("#teacher-username");
 
-				$("<span/>",
-				{
-					text: data.email
-				}).appendTo("#teacher-email");
+				// Profile email
+				if(data.email){
+					$("<span/>",
+					{
+						text: data.email
+					}).appendTo("#teacher-email");
+				}
+				else{
+					$("#teacher-email").toggleClass("hidden");
+				}
 
-				$("<span/>",
-				{
-					text: data.company
-				}).appendTo("#teacher-organization");
+				// Profile company
+				if(data.company){
+					$("<span/>",
+					{
+						text: data.company
+					}).appendTo("#teacher-organization");
+				}
+				else{
+					$("#teacher-organization").toggleClass("hidden");
+				}
 
-				$("<span/>",
-				{
-					text: data.location
-				}).appendTo("#teacher-location");
+
+				// Profile location
+				if(data.location){
+					$("<span/>",
+					{
+						text: data.location
+					}).appendTo("#teacher-location");
+				}
+				else{
+					$("#teacher-location").toggleClass("hidden");
+				}
 
 				$("<img/>",
 				{
